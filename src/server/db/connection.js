ca330c334410120604f359a3fa055e7ad32ae222
wujianlu <wjl51666@gmail.com>
2017-07-17 17:00:04 +0800

    delete .js

--- src/server/db/connection.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var knexfile_1 = require("../../../knexfile");
-var environment = process.env.NODE_ENV;
-exports.knex = new knexfile_1.knexConfig(environment).knex;

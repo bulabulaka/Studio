6f1f6a9af9a108077f73afaf83c9a9ad8b2f3638
wujianlu <wjl51666@gmail.com>
2017-07-21 17:56:54 +0800

    add component error-handle

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,16 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {AppComponent} from "./app.component";
+import {CanNotFindPageComponent, NoPermissionComponent} from "./error-handle/index";
+import {LoginComponent} from "./login/login.component"
 
-import { AppComponent } from './app.component';
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'no_permission',
+    component: NoPermissionComponent
+  },
+  {
+    path: '**',
+    component: CanNotFindPageComponent
+  }
+], {useHash: true});
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanNotFindPageComponent,
+    NoPermissionComponent,
+    LoginComponent
   ],
   imports: [
+    rootRouting,
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/client/app/error-handle/index.ts
@@ -0,0 +1,2 @@
+export * from "./can-not-find-page/can-not-find-page.component";
+export * from "./no-permission/no-permission.component";

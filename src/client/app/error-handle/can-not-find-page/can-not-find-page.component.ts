152895ce8ba6c66202515367052000fbf5c01401
wujianlu <wjl51666@gmail.com>
2017-07-25 18:40:34 +0800

    add components

---
+++ src/client/app/error-handle/can-not-find-page/can-not-find-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-can-not-find-page',
+  templateUrl: './can-not-find-page.component.html',
+  styleUrls: ['./can-not-find-page.component.css']
+})
+export class CanNotFindPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

5614fc089597fdfc678a219b1cb66a0797d15851
CONGYANG FU <congyangfu@FCY-Mac.local>
2017-07-27 17:09:17 +0800

    Bug Fix

--- src/server/db/connection.ts
+++ src/server/db/connection.ts
@@ -1,3 +1,3 @@
 import * as knexConfig from "../../../knexfile";
 const environment = process.env.NODE_ENV;
-export const knex = new knexConfig[environment].knex;
+export const knex =  knexConfig[environment].knex;

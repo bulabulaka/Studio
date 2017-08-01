814cccdf9b8683e0e9d7f1ee6b9822e6f9c20d9c
CONGYANG FU <congyangfu@FCY-Mac.local>
2017-08-01 10:21:40 +0800

    Export knex & extendedKnex

--- src/server/db/connection.ts
+++ src/server/db/connection.ts
@@ -1,16 +1,19 @@
-import * as knexConfig from '../../../knexfile';
-import * as Knex from 'knex';
+import * as knexConfig from "../../../knexfile";
+import * as Knex from "knex";
 import {Entities} from './entity/entityContainer';
 
-interface ExtendedKnex {
-  entities: Entities;
-  knex: Knex;
+interface ExtendedKnex{
+    entities:Entities;
+    knex:Knex;
+}
+const environment = process.env.NODE_ENV;
+
+export const knex =  Knex(knexConfig[environment]);
+
+
+export const extendKnex:ExtendedKnex = {
+    entities: new Entities(knex),
+    knex:knex
 }
 
-const environment = process.env.NODE_ENV;
-export const knex = Knex(knexConfig[environment]);
-export const extendKnex: ExtendedKnex = {
-  entities: new Entities(knex),
-  knex: knex
-};
-
+//export = extendKnex;

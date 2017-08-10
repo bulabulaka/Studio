984b101b0479965c8c015c90ec00c333743e375e
wujianlu <wjl51666@gmail.com>
2017-08-10 18:27:52 +0800

    change permission.service

--- src/client/app/shared/services/permission.service.ts
+++ src/client/app/shared/services/permission.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Rx';
 import {ApiService} from './api.service';
-import {m_permission, ResultValue} from '../../../../shared/index';
+import {permission, ResultValue} from '../../../../shared/index';
 
 @Injectable()
 export class PermissionService {
@@ -9,7 +9,7 @@ export class PermissionService {
   constructor(private apiService: ApiService) {
   }
 
-  addPermission(permission: m_permission): Observable<{ resultValue: ResultValue<number> }> {
+  addPermission(permission: permission): Observable<{ resultValue: ResultValue<number> }> {
     return this.apiService.post('/api/permission/add_permission', {permission: permission})
       .map(data => data)
   }
